26bb25ba2f075a2c5f4e0b5df124a88e32c8ef1d
tpotjj <tpotjj@users.noreply.github.com>
2023-04-20 06:59:32 +0000

    Format Terraform files

--- .tf/composition/api/eu-central-1/dev/main.tf
+++ .tf/composition/api/eu-central-1/dev/main.tf
@@ -15,7 +15,7 @@ module "ses_mail_server" {
   region   = var.region
 
   ## Route53 variables ##
-  route53_zone_id          = module.route53_dns_routing.route53_zone_id
+  route53_zone_id  = module.route53_dns_routing.route53_zone_id
   hosted_zone_name = var.route53_hosted_zone_name
 }
 
