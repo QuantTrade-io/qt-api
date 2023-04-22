b2b43fbb3a54ed22792a81cb6258dd867104235a
tpotjj <tpotjj@users.noreply.github.com>
2023-04-22 07:34:43 +0000

    Format Terraform files

--- .tf/modules/ses/data.tf
+++ .tf/modules/ses/data.tf
@@ -3,11 +3,11 @@
 ########################################
 
 locals {
-    ses_role_policy_name = "${lower(var.app_name)}-${var.env}-ses-mail"
-    ses_role_policy_path = "/"
-    ses_role_policy_description = "Allow sending email via SES"
+  ses_role_policy_name        = "${lower(var.app_name)}-${var.env}-ses-mail"
+  ses_role_policy_path        = "/"
+  ses_role_policy_description = "Allow sending email via SES"
 
-    ses_identity_policy_name = ""
+  ses_identity_policy_name = ""
 
   tags = merge(
     var.tags
